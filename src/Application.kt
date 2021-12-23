6ffdc4e0cfd1ead1ad40e6c00e5a68a74e6e263a
arcelorapp@123 <arcelorapp@gmail.com>
2021-12-23 09:44:59 +0530

    1st commit

---
+++ src/Application.kt
@@ -0,0 +1,56 @@
+package com.codexdroid
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import kotlinx.serialization.Serializable
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+
+@Serializable
+data class Person(var id:Int, var name:String, var email:String, var mobile:String)
+
+@Serializable
+data class DataList(var status:Int, var message:String?, var error:String?, var dataList: List<Person>?)
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    install(ContentNegotiation) {
+        gson {}
+    }
+
+    routing {
+
+        val dataList = mutableListOf<Person>()
+        repeat(30){ num ->
+
+            var firstName = ""
+            var lastName = ""
+
+            repeat(6){
+                firstName += Random.nextInt(97..122).toChar()
+            }
+            repeat(6){
+                lastName += Random.nextInt(97..122).toChar()
+            }
+
+            val email = "${firstName}@gmail.com"
+            val mobile = Random.nextLong(1111111111,9999999999).toString()
+
+            dataList.add(Person(id = num, name = "$firstName $lastName", email = email, mobile = mobile))
+        }
+
+        get("/data"){
+            call.respond(DataList(HttpStatusCode.OK.value,"All Data Fetch Successfully",null,dataList))
+        }
+    }
+}
+
+
+
